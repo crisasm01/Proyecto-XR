442bbc2156a07e5252caf3c212d6ff3d19d9c1ff
Cristian Sáez <cssaez@uc.cl>
2025-06-23 19:11:36 -0400

    Nueva entrega

--- Assets/FirstPersonLook.cs
+++ Assets/FirstPersonLook.cs
@@ -11,7 +11,6 @@ public class FirstPersonLook : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
